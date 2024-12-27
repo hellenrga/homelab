7e016bc3abd59cc1b46ebed3c086d3ba4c93dd14
hellenrga <hellenassis.dev@gmail.com>
2024-12-27 14:21:05 +0000

    script for automated backup creation with timestamped .tar.gz files.

---
+++ backup.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+
+diretorio_backup="/home/hellenrga/devops"
+nome_arquivo="backup_$(date +%Y%m%d_%H%M%S).tar.gz"
+tar -czf "$nome_arquivo" "$diretorio_backup"
+echo "Backup concluído em $nome_arquivo"
+
