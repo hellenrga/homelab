c7404c3588dc3ecbe7861b7810b90935677d0c1d
Hellen Assis <79284436+hellenrga@users.noreply.github.com>
2025-01-04 10:20:06 -0300

    Update and rename convertimg.sh to converting.sh

--- ubuntuservervm/converting.sh
+++ ubuntuservervm/converting.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Solicitamos a indicação do caminho do diretório
-read -p "Digite o caminho do diretório com as imagens JPG: " diretorio
+read -p "Enter the directory path with the JPG images: " diretorio
 
 # Verificamos se o diretório indicado existe
 if [ ! -d "$diretorio" ]; then
@@ -14,4 +14,4 @@ for imagem_jpg in "$diretorio"/*.jpg; do
     convert "$imagem_jpg" "${imagem_jpg%.jpg}.png" && echo "Imagem convertida: ${imagem_jpg%.jpg}.png" || echo "Falha na conversão: $imagem_jpg"
 done
 
-echo "Conversão concluída!"
+echo "Conversion Concluded!"
